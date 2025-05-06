3a8f6c4df4fba96ba83f2205f8cd5e5799d8bf08
Erick Diaz Cerdas <erick.diaz@babelgroup.com>
2025-05-06 10:28:42 -0600

    Add pull request test

--- test/can-reconfigure.test.js
+++ test/can-reconfigure.test.js
@@ -12,7 +12,7 @@ describe("canReconfigure", () => {
   });
 
   it("should throw if first parameter is not a string", () => {
-    expect(() => canReconfigure(2)).toThrow("from must be a string");
+    expect(() => canReconfigure(4)).toThrow("from must be a string");
   });
 
   it("should throw if second parameter is not a string", () => {
